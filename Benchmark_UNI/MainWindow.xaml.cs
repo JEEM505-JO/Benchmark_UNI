0a4826ff903e9d086e2fd99c4034f70dfcfcb404
jespinoza <jespinoza@empresa.thedataage.com>
2022-04-14 17:50:04 -0600

    Comentarios agregados sobre tiempo de ejecucion

--- Benchmark_UNI/MainWindow.xaml.cs
+++ Benchmark_UNI/MainWindow.xaml.cs
@@ -1,19 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Diagnostics;
-using System.Threading;
 
 namespace Benchmark_UNI
 {
@@ -26,10 +12,11 @@ namespace Benchmark_UNI
         {
             InitializeComponent();
         }
-      
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //inicio de reloj
             Stopwatch sw = Stopwatch.StartNew();
             int n1, n2, result;
             double result2;
@@ -40,13 +27,18 @@ namespace Benchmark_UNI
             result = n1 + n2;
 
             resultsuma.Text = result.ToString();
+            
+            //sw.stop detiene el reloj luego del algoritmo, se pone al final
+            //y se manda a mostrar luego de calcular los datos
             sw.Stop();
+            //muestra los datos en milisegundos
             result2 = sw.Elapsed.TotalMilliseconds;
 
             resultime.Text = result2.ToString();
-            
+
         }
 
+        //limpia los textbox
         private void limpiar(object sender, RoutedEventArgs e)
         {
             number1.Text = " ";
